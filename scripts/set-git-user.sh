c6950e95c8d87af49cec2c9afbd88aa5d6bb2dec
bugbiteme <leon.s.levy@gmail.com>
2026-03-16 16:46:40 +0000

    helper script

---
+++ scripts/set-git-user.sh
@@ -0,0 +1,3 @@
+#helper script to set git user name and email (optional) in case running on fresh bastion host
+git config --global user.email "leon.s.levy@gmail.com"
+git config --global user.name "bugbiteme"
